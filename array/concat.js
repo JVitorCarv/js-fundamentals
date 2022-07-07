89f10942a02fd13611b0d9fd46f3afc546ab7933
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-07-07 13:49:39 -0300

    Create concat and flatMap

---
+++ array/concat.js
@@ -0,0 +1,5 @@
+const array1 = [1, 2];
+const array2 = [3, 4];
+
+const all = array1.concat(array2);
+console.log(all);
---
+++ array/flatMap.js
@@ -0,0 +1,41 @@
+const school = [
+	{
+		name: "Classroom N1",
+		students: [
+			{
+				name: "Bob",
+				grade: 8.1
+			},
+			{
+				name: "Martin",
+				grade: 9.3
+			}
+		]
+	},
+	{
+		name: "Classroom N2",
+		students: [
+			{
+				name: "Kent",
+				grade: 8.9
+			},
+			{
+				name: "Fred",
+				grade: 7.3
+			}
+		]
+	}
+];
+
+const getStudentGrade = (s) => s.grade;
+const getClassroomGrades = (c) => c.students.map(getStudentGrade);
+
+const grades1 = school.map(getClassroomGrades);
+console.log(grades1);
+
+Array.prototype.flatMap = function (callback) {
+	return Array.prototype.concat.apply([], this.map(callback));
+};
+
+const grades2 = school.flatMap(getClassroomGrades);
+console.log(grades2);
