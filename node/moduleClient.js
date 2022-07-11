f80180f15a1fbb906e82f73aed7eeff617003796
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-07-11 15:33:06 -0300

    Node module system

---
+++ node/moduleClient.js
@@ -0,0 +1,7 @@
+const moduleA = require("./moduleA");
+const moduleB = require("./moduleB");
+
+console.log(moduleA.welcome);
+console.log(moduleA);
+console.log(moduleB.goodNight());
+console.log(moduleB);
