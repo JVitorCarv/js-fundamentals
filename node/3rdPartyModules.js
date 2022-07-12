3edd7fe825210c2a0a70ff67925eb7bbd3c61ea0
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-07-12 11:17:27 -0300

    Use http

--- node/3rdPartyModules.js
+++ node/3rdPartyModules.js
@@ -1,2 +1,11 @@
 const _ = require("lodash");
-setInterval(() => console.log(_.random(500, 1000)), 1000);
+//setInterval(() => console.log(_.random(500, 1000)), 1000);
+
+const http = require("http"); // node has this module
+
+http
+	.createServer((request, callback) => {
+		callback.write("Bom dia!");
+		callback.end();
+	})
+	.listen(8080);
